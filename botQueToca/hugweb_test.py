3e2e514af9733ec288a1da57c838f8722f8571fe
David Sanchez <anixo.a.tope@gmail.com>
2017-11-07 02:26:46 +0100

    Añadido test ruta

--- botQueToca/hugweb_test.py
+++ botQueToca/hugweb_test.py
@@ -2,6 +2,11 @@ import hug
 import hugweb
 
 def test_estado_OK():
-    estado = hug.test.get(hugweb, '/')
-    assert estado.status == "200 OK"
-    assert estado.data['status']=="OK"
+    datos = hug.test.get(hugweb, '/')
+    assert datos.status == "200 OK"
+    assert datos.data['status']=="OK"
+
+def test_ruta_correcta():
+    datos = hug.test.get(hugweb, '/horario')
+    assert datos.status == "200 OK"
+    assert datos.data['Horario'] == "[(4, 'A', 'Infraestructura virtual', datetime.date(2016, 11, 6), -1.2, datetime.time(9, 30), datetime.time(11, 30), 'JJ')]"
