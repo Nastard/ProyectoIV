7a4c3f59a3a4b8c95a43f2deb7314f09bdbf6521
David Sanchez <anixo.a.tope@gmail.com>
2018-01-05 21:33:30 +0100

    cambiado archivos

--- botQueToca/hugweb.py
+++ botQueToca/hugweb.py
@@ -1,7 +1,7 @@
 import hug
-from QueTocaBBDD import Horario
+# from QueTocaBBDD import Horario
 
-horario=Horario()
+# horario=Horario()
 
 @hug.get('/')
 def index():
@@ -13,10 +13,10 @@ def status():
 	"""Devuelve estado"""
 	return { "status": "OK" }
 
-@hug.get('/horario')
-def actividades_bd():
-	info = horario.LeerHorario("D1")
-	return { "Horario": info }
+# @hug.get('/horario')
+# def actividades_bd():
+# 	info = horario.LeerHorario("D1")
+# 	return { "Horario": info }
 
 # @hug.get('/unhorario/{id}')
 # def one ( id: int ):
--- botQueToca/hugweb_test.py
+++ botQueToca/hugweb_test.py
@@ -11,10 +11,9 @@ def test_estado_OK():
     assert datos.status == "200 OK"
     assert datos.data['status']=="OK"
 
-def test_ruta_correcta():
-    datos = hug.test.get(hugweb, '/horario')
-    assert datos.status == "200 OK"
-    # assert datos.data['Horario'] == "[(4, 'A', 'Infraestructura virtual', datetime.date(2016, 11, 6), -1.2, datetime.time(9, 30), datetime.time(11, 30), 'JJ')]"
+# def test_ruta_correcta():
+#     datos = hug.test.get(hugweb, '/horario')
+#     assert datos.status == "200 OK"
 
 # def test_ruta_un_elemento():
 #     datos = hug.test.get(hugweb, '/unhorario/4')
