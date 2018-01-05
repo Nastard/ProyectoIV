d6be9081aabbb7e07a206d6c12e7c866df6e3f7b
David Sanchez <anixo.a.tope@gmail.com>
2018-01-05 22:35:16 +0100

    Cambios finales

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -2,11 +2,13 @@ from fabric.api import run, sudo, env, shell_env
 import os
 
 def Instalar():
+	sudo("apt-get install -y supervisor")
 	run("git clone https://github.com/Anixo/ProyectoIV")
+	sudo("cp /home/vagrant/ProyectoIV/supervisor.conf /etc/supervisor/conf.d/")
 	run("pip3 install -r /home/vagrant/ProyectoIV/requirements.txt")
 
 def Desinstalar():
-	run("rm -R -f /home/vagrant/ProyectoIV")
+	sudo("rm -R -f /home/vagrant/ProyectoIV")
 
 def Iniciar():
-	sudo("hug -p 80 -f /home/vagrant/ProyectoIV/botQueToca/hugweb.py")
+	sudo("cd /home/vagrant/ProyectoIV/ && supervisorctl reread && supervisorctl reload && supervisorctl start hugweb", pty = False)
