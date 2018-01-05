e8863c36f20c12c9f867051c4df28985cda999b5
David Sanchez <anixo.a.tope@gmail.com>
2018-01-05 22:49:52 +0100

    Cambios finales

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -4,7 +4,7 @@ import os
 def Instalar():
 	sudo("apt-get install -y supervisor")
 	run("git clone https://github.com/Anixo/ProyectoIV")
-	sudo("cp /home/vagrant/ProyectoIV/supervisor.conf /etc/supervisor/conf.d/")
+	sudo("cp /home/vagrant/ProyectoIV/hugwebconfig.conf /etc/supervisor/conf.d/")
 	run("pip3 install -r /home/vagrant/ProyectoIV/requirements.txt")
 
 def Desinstalar():
